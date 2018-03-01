2054bee6d96c6ecda640b76d90b0604a638f3027
alylagayan <alylagayan@gmail.com>
2018-03-01 14:36:20 +0800

    [DB] Added some sample data for `users

--- SECURDE_DB.sql
+++ SECURDE_DB.sql
@@ -11,7 +11,7 @@ CREATE table users(
     username VARCHAR(25) NOT NULL,
     password VARCHAR(80) NOT NULL,
     email TEXT NOT NULL,
-    contact_no INT NOT NULL,
+    contact_no VARCHAR(11) NOT NULL,
     address LONGTEXT NOT NULL,
     shop_id INT NULL,
     
@@ -23,13 +23,11 @@ CREATE table users(
 -- A new row in `shops` is created when a user opens a shop
 
 CREATE table shops(
-	id INT AUTO_INCREMENT NOT NULL,
+	id INT NOT NULL,
     shop_owner INT NOT NULL,
-    shop_name TEXT,
-    shop_address LONGTEXT NOT NULL,
-    shop_email TEXT NOT NULL,
-    shop_contact INT NOT NULL,
-    description VARCHAR(300),
+    shop_name TEXT NOT NULL,
+    shop_contact VARCHAR(11) NOT NULL,
+    description VARCHAR(300) null,
 
 	PRIMARY KEY (id),
 
@@ -77,3 +75,11 @@ CREATE table product_status(
 	status_id INT,
     state TEXT
 );
+
+INSERT INTO `users`(`username`,`password`,`email`,`contact_no`,`address`) 
+	VALUES('1_admin','01_admin','alylagayan@gmail.com','09471303432','B24 L14 P1A San Lorenzo South, Sta. Rosa, Laguna, Philipppines'),
+		  ('rossmendez','hOSANNA','rossmendez@gmail.com','0922996193','Muralla Industrial Park, Libtong, Philipppines'),
+          ('doktoraBello','uyBESH','bello_phd@gmail.com','0922996193','SUITE 202 Cortijos Condo25 Eisenhower Street Greenhills, Philipppines'),
+          ('cass_ce','bebebebe','c_matias@yahoo.com','09893212253','7/F The Athenaeum Building, 160 Alfaro Street Salcedo Village 1200, Makati City, Manila, Philipppines');
+INSERT INTO `users`(`username`,`password`,`email`,`contact_no`,`address`,`shop_id`) 
+	VALUES('3_admin','03_admin','jan_lagayan@dlsu.edu.ph','09471303432','B24 L14 P1A San Lorenzo South, Sta. Rosa, Laguna, Philipppines', 1);
