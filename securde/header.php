113fc90c7c8431231b155391e0da41028434a236
alylagayan <alylagayan@gmail.com>
2018-03-19 20:11:53 +0800

    Started out on the PHP stuff
    All of them are on the 'securde' folder

---
+++ securde/header.php
@@ -0,0 +1,32 @@
+<?php
+
+
+?>
+
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="site_css.css">
+		<title>CraftCourt</title>
+	</head>
+	
+	<body>
+		<div class="session">
+			<a href="">Login</a>
+			<a href="">Register</a>
+			<a href="">Cart</a>
+		</div>
+		<div class="header">
+			<span>CraftCourt</span> <input type="text" placeholder="Search for..."> 
+		</div>
+		
+		<div class="categories">
+			<a href="">Accessories</a>
+			<a href="">Clothing</a>
+			<a href="">Party</a>
+			<a href="">Entertainment</a>
+			<a href="">Tools</a>
+		</div>
+		
+		<br>
+		
+		
---
+++ securde/index.php
@@ -0,0 +1,31 @@
+<?php
+	include 'header.php';
+?>
+
+<div class="list">
+			<div class="item"> 
+				<div class = "item_pic">
+				<img src="chair_1.jpg" style="height: 170; width: 170; padding: 15px;"></div>
+				<div class = "item_desc">
+					<p>Item 1</p>
+					<p>Pencil case</p>
+					<p>PHP 90.00</p>
+				</div>
+
+			</div>
+			<br>
+			<div class="item"> <div class = "item_pic">pic_2</div>
+				<p>Item 2</p>
+				<p>Peacoat</p>
+				<p>PHP 390.00</p>
+			</div>
+			<br>
+			<div class="item"> <div class = "item_pic">pic_3</div>
+				<p>Item 3</p>
+				<p>Sketchpad</p>
+				<p>PHP 100.00</p>
+			</div>
+		</div>
+	</body>
+	
+</html>
