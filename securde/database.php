7148a5b16319347a5f0883efc0f61a3e80586668
alylagayan <alylagayan@gmail.com>
2018-03-27 10:02:18 +0800

    [PHP/HTML] New Pages

---
+++ securde/database.php
@@ -0,0 +1,9 @@
+ <?php
+	$servername = "localhost";
+	$username = "root";
+	$password = "e01_bright";
+	$dbname = "craftcourt_db";
+
+	// Create connection
+	$conn = mysqli_connect($servername, $username, $password, $dbname);
+?> 
--- securde/index.php
+++ securde/index.php
@@ -2,30 +2,48 @@
 	include 'header.php';
 ?>
 
-<div class="list">
-			<div class="item"> 
-				<div class = "item_pic">
-				<img src="chair_1.jpg" style="height: 170; width: 170; padding: 15px;"></div>
-				<div class = "item_desc">
-					<p>Item 1</p>
-					<p>Pencil case</p>
-					<p>PHP 90.00</p>
-				</div>
+	<?php
+		$select_product = "SELECT * FROM products;";
+		
+		$result = $conn->query($select_product);
 
-			</div>
-			<br>
-			<div class="item"> <div class = "item_pic">pic_2</div>
-				<p>Item 2</p>
-				<p>Peacoat</p>
-				<p>PHP 390.00</p>
-			</div>
-			<br>
-			<div class="item"> <div class = "item_pic">pic_3</div>
-				<p>Item 3</p>
-				<p>Sketchpad</p>
-				<p>PHP 100.00</p>
-			</div>
-		</div>
-	</body>
-	
-</html>
+		if ($result->num_rows > 0) {
+			
+			echo "<div class='list-group'>";
+			
+			while($row = $result->fetch_assoc()) {
+				echo "<a href='/product?title=".$row["title"]."' class='list-group-item'>";
+				echo "<h4 class='list-group-item-heading'>".$row["title"]."</h4>";
+				echo "<p class='list-group-item-text'>".$row["description"]."</p>";
+				echo "<p class='list-group-item-text'>₱".$row["price"]."</p>";
+				echo "</a>";
+			}
+			
+			echo "</div>";
+			
+		} else {
+			echo "0 results";
+		}
+	?>
+
+<?php
+	include 'footer.php';
+?>
+
+<!--
+
+<div class="list-group">
+    <a href="#" class="list-group-item active">
+      <h4 class="list-group-item-heading">First List Group Item Heading</h4>
+      <p class="list-group-item-text">List Group Item Text</p>
+    </a>
+    <a href="#" class="list-group-item">
+      <h4 class="list-group-item-heading">Second List Group Item Heading</h4>
+      <p class="list-group-item-text">List Group Item Text</p>
+    </a>
+    <a href="#" class="list-group-item">
+      <h4 class="list-group-item-heading">Third List Group Item Heading</h4>
+      <p class="list-group-item-text">List Group Item Text</p>
+    </a>
+  </div>
+-->
