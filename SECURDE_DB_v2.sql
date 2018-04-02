163bd6dc997c2f491b4227c3faa0a2f549bfcfe8
alylagayan <alylagayan@gmail.com>
2018-04-02 21:57:26 +0800

    [DB] MADE IT WORK AGAIN
    AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

--- SECURDE_DB_v2.sql
+++ SECURDE_DB_v2.sql
@@ -6,6 +6,29 @@ USE craftcourt_db;
 -- Creating tables...
 -- ==============================================================
 
+-- TURNS OUT I NEED TO MAKE PK B4 I MAKE IT FK...GODS HELP ME
+
+CREATE table p_payment(
+	py_id INT NOT NULL,
+    py_mode VARCHAR(200) NOT NULL,
+    
+    PRIMARY KEY(py_id)
+);
+
+CREATE table p_status(
+	st_id INT NOT NULL,
+    status_txt VARCHAR(200) NOT NULL,
+    
+    PRIMARY KEY(st_id)
+);
+
+CREATE table p_type(
+	t_id INT NOT NULL,
+    type_txt VARCHAR(200) NOT NULL,
+    
+    PRIMARY KEY(t_id)
+);
+
 CREATE table users(
 	id INT AUTO_INCREMENT NOT NULL,
     uname VARCHAR(50) NOT NULL,
@@ -31,11 +54,11 @@ CREATE table user_shops(
     
     CONSTRAINT keeper
 		FOREIGN KEY (id)
-        REFERENCES users(id),
+        REFERENCES `users`(`id`),
         
     CONSTRAINT shop
 		FOREIGN KEY (s_id) 
-        REFERENCES shops(s_id)
+        REFERENCES `shops`(`s_id`)
 );
 
 CREATE table products(
@@ -44,64 +67,53 @@ CREATE table products(
     title VARCHAR(100) NOT NULL,
     price decimal(8,2) NOT NULL,
     description VARCHAR(250) NULL,
-    state INT NOT NULL,
     p_type INT NOT NULL,
+    state INT NOT NULL,
     
     CONSTRAINT provider
-		FOREIGN KEY (seller)
-        REFERENCES shops(s_id),
-    CONSTRAINT existence
-		FOREIGN KEY (state)
-		REFERENCES p_status(st_id),
-    
+		FOREIGN KEY (seller) 
+        REFERENCES `shops`(`s_id`),
+        
+	CONSTRAINT state
+		FOREIGN KEY(state)
+        REFERENCES p_status(st_id),
+        
     PRIMARY KEY (p_id)
 );
 
-CREATE table listing(
-	l_id INT AUTO_INCREMENT NOT NULL,
-    listdate DATE NOT NULL,
-    p_id INT NOT NULL,
-    quantity INT NOT NULL,
+-- CREATE table listing(
+-- 	l_id INT AUTO_INCREMENT NOT NULL,
+-- 	listdate DATE NOT NULL,
+--    p_id INT NOT NULL,
+--    quantity INT NOT NULL,
     
-    CONSTRAINT product
-		FOREIGN KEY (p_id)
-        REFERENCES products(p_id),
+--    CONSTRAINT product
+-- 		FOREIGN KEY (p_id)
+--        REFERENCES products(p_id),
         
-    PRIMARY KEY (l_id)
-);
+--    PRIMARY KEY (l_id)
+-- );
 
-CREATE table deliveries(
-	d_id INT AUTO_INCREMENT NOT NULL,
+ CREATE table deliveries(
+ 	d_id INT AUTO_INCREMENT NOT NULL,
     p_id INT NOT NULL,
     saledate DATE NOT NULL,
-    fullname VARCHAR(200) NOT NULL,
-    address TEXT,
-    payment_method INT NOT NULL,
+	fullname VARCHAR(200) NOT NULL,
+	address TEXT NOT NULL,
+    py_id INT NOT NULL,
     
-    CONSTRAINT product
+    CONSTRAINT bought
 		FOREIGN KEY (p_id)
-        REFERENCES products(p_id),
-	CONSTRAINT payment
-		FOREIGN KEY (payment_method)
-        REFERENCES payment(py_id),
+		REFERENCES `products`(`p_id`),
         
-	PRIMARY KEY (d_id)
-);
+	CONSTRAINT pmode
+		FOREIGN KEY (py_id)
+		REFERENCES `p_payment`(`py_id`),
+        
+	 	PRIMARY KEY (d_id)
+ );
 
-CREATE table payment(
-	py_id INT,
-    py_mode VARCHAR(10)
-);
 
-CREATE table p_status(
-	st_id INT,
-    status_txt TEXT
-);
-
-CREATE table p_type(
-	t_id INT,
-    type_txt TEXT
-);
 -- ==============================================================
 -- Adding sample data...
 -- ==============================================================
@@ -127,7 +139,7 @@ INSERT INTO `p_status`
 	VALUES	(1,'For Sale'),
 			(2,'Sold');
             
-INSERT INTO `p_status`
+INSERT INTO `p_payment`
 	VALUES	(1,'Paypal'),
 			(2,'Credit Card');
     
@@ -157,3 +169,4 @@ INSERT INTO `products`(`seller`,`title`,`price`,`description`, `state`,`p_type`)
 			(2, 'Vanguard Deletor mat', 500.00, 'used few times', 1, 1),
             (3, 'God of War collection', 600.00, 'PSVita game', 1, 14),
 			(2, 'Vanguard Dimensional Robo deck', 4000.00, 'Comes with sleeves and deckbox', 1, 14);
+            
