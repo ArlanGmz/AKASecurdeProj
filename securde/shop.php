e2680cd9ea183bcb2cae5e4335744318c4609736
alylagayan <alylagayan@gmail.com>
2018-04-11 21:51:14 +0800

    [WEB] changed shop

--- securde/shop.php
+++ securde/shop.php
@@ -24,8 +24,8 @@
 	echo "<div class='well well-lg'>";
 	echo "<h3 class='text-left'>".$s_name."</h3>";
 	echo "<p class='float-right'>Owner: ".$s_owner."</p>";
-	echo "<small>".$s_desc."</small>";
-	echo "<a href='cart_shipping.php?title=<?php echo $_SESSION['product'];?>' class='btn btn-info' role='button'>Edit</a>" 
+	echo "<small>".$s_desc."</small><br>";
+	echo "<a href='' class='btn btn-info' role='button'>Edit</a>" ;
 	echo "</div>";
 	/*The shop's products*/
 	
@@ -39,9 +39,41 @@
 				echo "<a href='product.php?title=".$row["p_id"]."' class='list-group-item'>";
 				echo "<h4 class='list-group-item-heading'>".$row["title"]."</h4>";
 				echo "<p class='list-group-item-text'>₱".$row["price"]."</p>";
-				echo "<a href='cart_shipping.php?title=<?php echo $_SESSION['product'];?>' class='btn btn-info' role='button'>Edit</a>" 
-				echo "<a href='cart_shipping.php?title=<?php echo $_SESSION['product'];?>' class='btn btn-info' role='button'>Remove</a>" 
+				echo "<a href='#editModal' class='btn btn-info' role='button' data-toggle='modal'><span class='glyphicon glyphicon-cog'></span></a>" ;
+				echo "<a href='#deleteModal' class='btn btn-info' role='button' data-toggle='modal'><span class='glyphicon glyphicon-remove'></span></a>"; 
 				echo "</a>";
+?>				
+	<div id="editModal" class="modal fade">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+            <h4 class="modal-title">Edit Item</h4>
+          </div>
+          <div class="modal-body">
+            <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]."?shop=".$_SESSION["shop"]);?>">
+						<div class="form-group">
+							<label for="i_name">Item Name:</label>
+							<input type="text" class="form-control" id="i_name" name="i_name">
+						</div>
+						<div class="form-group">
+							<label for="i_desc">Description:</label>
+							<input type="text" class="form-control" id="i_desc" name="i_desc">
+						</div>
+						<div class="form-group">
+							<input type="hidden" class="form-control" name="i_num" value=<?php echo $row["p_id"];?>>
+						</div>
+			 
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            <button type="submit" class="btn btn-primary">Save changes</button>
+			</form>
+          </div>
+        </div>
+      </div>
+    </div>
+<?php
 			}
 			
 			echo "</div>";
