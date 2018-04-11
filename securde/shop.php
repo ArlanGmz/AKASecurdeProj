91436b70332704aaf1d79afb688a7cd494789c58
alylagayan <alylagayan@gmail.com>
2018-04-11 23:25:12 +0800

    now we can delete products

--- securde/shop.php
+++ securde/shop.php
@@ -12,12 +12,23 @@
 			$change->bind_param("s", $n);
 			$n = $_POST["i_name"];
 			$change->execute();
+			unset($_POST["i_name"]);
 		}else if(!empty($_POST["i_desc"])){
 			$change = $conn->prepare("UPDATE products SET description=? WHERE p_id=".$_POST["i_num"]);
 			$change->bind_param("s", $d);
 			$d = $_POST["i_desc"];
 			$change->execute();
+			unset($_POST["i_desc"]);
 		}
+	}
+	
+	/*Whe someone deletes*/
+	if(isset($_POST["delete_num"])){
+		$delete = $conn->prepare("DELETE from products WHERE p_id= ?");
+		$delete->bind_param("i", $d);
+		$d = $_POST["delete_num"];
+		$delete->execute();
+		unset($_POST["delete_num"]);
 	}
 	/*
 	$stmt = $conn->prepare("INSERT INTO MyGuests (firstname, lastname, email) VALUES (?, ?, ?)");
@@ -68,7 +79,8 @@ $stmt->execute();
 				echo "<a href='#editModal' class='btn btn-info' role='button' data-toggle='modal'><span class='glyphicon glyphicon-cog'></span></a>" ;
 				echo "<a href='#deleteModal' class='btn btn-info' role='button' data-toggle='modal'><span class='glyphicon glyphicon-remove'></span></a>"; 
 				echo "</a>";
-?>				
+?>		
+	<!-- Edit Modal-->
 	<div id="editModal" class="modal fade">
       <div class="modal-dialog">
         <div class="modal-content">
@@ -94,6 +106,29 @@ $stmt->execute();
           <div class="modal-footer">
             <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
             <button type="submit" class="btn btn-primary">Save changes</button>
+			</form>
+          </div>
+        </div>
+      </div>
+    </div>
+	<!-- Delete Modal-->
+	<div id="deleteModal" class="modal fade">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+            <h4 class="modal-title">Edit Item</h4>
+          </div>
+          <div class="modal-body">
+            <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]."?shop=".$_SESSION["shop"]);?>">
+						<p>Are you sure you want to delete this item?</p>
+						<div class="form-group">
+							<input type="hidden" class="form-control" name="delete_num" value=<?php echo $row["p_id"];?>>
+						</div> 
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">No</button>
+            <button type="submit" class="btn btn-primary">Yes</button>
 			</form>
           </div>
         </div>
