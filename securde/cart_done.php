3566c20ecac3ad38dfe5ce3dabfe1ec57df4b695
alylagayan <alylagayan@gmail.com>
2018-04-11 22:19:57 +0800

    Revert "Merge pull request #2 from ArlanGmz/ohdear"
    This reverts commit 9205d40b0b43ee1c6ba209665fa0238704ba14d7, reversing
    changes made to ac65b5af87638dcbbd7b2e746a684e6fc864681a.

--- securde/cart_done.php
+++ securde/cart_done.php
@@ -3,12 +3,7 @@
 	if(isset($_SESSION["product"]) == FALSE){
 		echo 'No item selected.';
 	}else{
-		$paymode = 0;
 		
-<<<<<<< HEAD
-		if(strcmp ( $_POST["paytype"] , "cash" ) == 0){$paymode = 1;}
-		if(strcmp ( $_POST["paytype"] , "card" ) == 0){$paymode = 2;}
-=======
 	$sql = "UPDATE products SET state='2' WHERE id=".$_SESSION["product"];
 
 		if ($conn->query($sql) === TRUE) {
@@ -30,27 +25,10 @@
 	$stmt->execute();
 	
 	}
->>>>>>> ac65b5af87638dcbbd7b2e746a684e6fc864681a
 		
-		$override = $conn->prepare("UPDATE products SET state='2' WHERE id= ? ");
-		$override->bind_param("i", $mode);
-		$mode = $_SESSION["product"];
-		$override->execute();
-		
-		$adddelivery= $conn->prepare("INSERT INTO deliveries (p_id, saledate, fullname, address, py_id)VALUES (?,?,?,?,?)");
-		$adddelivery->bind_param("isssi", $product, $date, $name, $address, $payment);
-		$product=$_SESSION["product"];
-		$date= date("Y/m/d");
-		$name= $_SESSION["fullname"];
-		$address= $_SESSION["address"]; 
-		$payment= $paymode;
-		$adddelivery->execute();
-	}
-		?>
+	
+?>
 
-	<div class="alert alert-success">
-	  <strong>Success!</strong> You should <a href="#" class="alert-link">read this message</a>.
-	</div>
 
 <?php
 	
